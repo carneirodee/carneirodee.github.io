5299970589277ade20405364a6cc4de66697a6cd
carneirodee <carneirodeyse0@gmail.com>
2024-07-02 16:18:14 -0300

    REFACTORING SECTIONS PAGE

--- src/components/ScrollSnapPage/index.js
+++ src/components/ScrollSnapPage/index.js
@@ -1,6 +1,5 @@
 import Container from '../Container';
 import Content from '../Content';
-import ScrollProgressDots from '../ScrollProgressDots';
 import {
     SectionAbout,
     SectionContact,
@@ -25,33 +24,25 @@ function ScrollSnapPage() {
     return (
 
         <Container>
-            <ScrollProgressDots
-                isHome={isHome}
-                // isAbout={isAbout}
-                isServices={isServices}
-                // isProjects={isProjects}
-                // isExperience={isExperience}
-                isContact={isContact}
-            />
             <Content>
-                <VisibilitySensor onChange={(isVisible) => { setIsHome(isVisible) }}>
+                {/* <VisibilitySensor onChange={(isVisible) => { setIsHome(isVisible) }}> */}
                     <SectionHome />
-                </VisibilitySensor>
+                {/* </VisibilitySensor> */}
                 {/* <VisibilitySensor onChange={(isVisible) => { setIsAbout(isVisible) }}>
                     <SectionAbout> <h1> About </h1></SectionAbout>
                 </VisibilitySensor> */}
-                <VisibilitySensor onChange={(isVisible) => { setIsServices(isVisible) }}>
+                {/* <VisibilitySensor onChange={(isVisible) => { setIsServices(isVisible) }}> */}
                     <SectionServices></SectionServices>
-                </VisibilitySensor>
+                {/* </VisibilitySensor> */}
                 {/* <VisibilitySensor onChange={(isVisible) => { setIsProjects(isVisible) }}>
                     <SectionProjects> <h1> Projects </h1></SectionProjects>
                 </VisibilitySensor> */}
                 {/* <VisibilitySensor onChange={(isVisible) => { setIsExperience(isVisible) }}>
                     <SectionExperience> <h1> Experience </h1></SectionExperience>
                 </VisibilitySensor> */}
-                <VisibilitySensor onChange={(isVisible) => { setIsContact(isVisible) }}>
+                {/* <VisibilitySensor onChange={(isVisible) => { setIsContact(isVisible) }}> */}
                     <SectionContact> <h1> Contact </h1></SectionContact>
-                </VisibilitySensor>
+                {/* </VisibilitySensor> */}
             </Content>
         </Container>
     );
